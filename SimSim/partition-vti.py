a554c11f85a1e2f752fa35cf382c152c5c86739b
Gregory Abram <gda@c262-224.hikari.tacc.utexas.edu>
2017-01-23 11:55:19 -0600

    select variable

--- SimSim/partition-vti.py
+++ SimSim/partition-vti.py
@@ -3,19 +3,29 @@
 import sys
 from vtk import *
 
-w = vtkXMLImageDataReader()
-w.SetFileName(sys.argv[1])
-w.Update()
-
 parts = [2, 2, 2]
 
-extent = w.GetOutput().GetExtent()
+r = vtkXMLImageDataReader()
+r.SetFileName(sys.argv[1])
+
+if (len(sys.argv) > 2):
+  r.UpdateInformation()
+  for i in range(r.GetNumberOfPointArrays()):
+    n = r.GetPointArrayName(i)
+    if n in sys.argv[2:]:
+      r.SetPointArrayStatus(n, 1)
+    else:
+      r.SetPointArrayStatus(n, 0)
+
+r.Update()
+
+extent = r.GetOutput().GetExtent()
 origin = [extent[2*i] for i in range(3)]
 span   = [extent[2*i + 1] - extent[2*i] for i in range(3)]
 step  = [span[i] / parts[i] for i in range(3)]
 
 clp = vtkImageClip()
-clp.SetInputConnection(w.GetOutputPort())
+clp.SetInputConnection(r.GetOutputPort())
 clp.ClipDataOn()
 
 wrtr = vtkXMLImageDataWriter()
@@ -36,8 +46,3 @@ for i in range(2):
 			wrtr.SetFileName('%s-%d.vti' % (sys.argv[1].rsplit('.',1)[0], p))
 			wrtr.Update()
 			p = p + 1
-
-
-
-
-
